2e4c575c26f3621e624a92457ba44b33f1863365
Mon Loup <monloup@Benedictes-MacBook-Air.local>
2014-11-03 12:30:10 +0100

    Admin Agent Form

--- atp/agent/admin.py
+++ atp/agent/admin.py
@@ -4,9 +4,64 @@ from users.models import User
 
 # Register your models here.
 
+class CertificationsInline(admin.StackedInline):
+    model = AgentCertification
+    suit_classes = 'suit-tab suit-tab-certifications'
+
+
+class AgentAddressInline(admin.StackedInline):
+    model = AgentAddress
+    max_num = 1
+    extra = 0
+    suit_classes = 'suit-tab suit-tab-address'
+
+    def has_delete_permission(self, request, obj=None):
+            return False
+
+
+class AgentIdCardInline(admin.StackedInline):
+    model = AgentIdCard
+    max_num = 1
+    extra = 0
+    suit_classes = 'suit-tab suit-tab-id_card'
+
+    def has_delete_permission(self, request, obj=None):
+            return False
+
+
+class AgentProCardInline(admin.StackedInline):
+    model = AgentProCard
+    max_num = 1
+    extra = 0
+    suit_classes = 'suit-tab suit-tab-pro_card'
+
+    def has_delete_permission(self, request, obj=None):
+            return False
+
 
 class AgentAdmin(admin.ModelAdmin):
-    pass
+    inlines = (CertificationsInline, AgentIdCardInline, AgentAddressInline, AgentProCardInline)
+
+    fieldsets = [
+            (None, {
+                'classes': ('suit-tab', 'suit-tab-etat_civil'),
+                'fields': ['genre', 'nationality', 'birthdate', 'birthplace']
+                }),
+            ('Papiers identite', {
+                'classes': ('suit-tab', 'suit-tab-id_card'),
+                'fields': ['user']
+                }),
+            ('Adresse', {
+                'classes': ('suit-tab', 'suit-tab-address'),
+                'fields': ['birthplace']
+                }),
+            ('Carte Pro', {
+                'classes': ('suit-tab', 'suit-tab-pro_card'),
+                'fields': ['user']
+                }),
+            ]
+    suit_form_tabs = (('etat_civil', 'Etat civil'), ('id_card', 'Papiers identite'), ('address', 'Coordonnees'), ('pro_card', 'Carte Pro'), ('certifications', 'Certifications'))
+
 
 
 class CertificationAdmin(admin.ModelAdmin):
